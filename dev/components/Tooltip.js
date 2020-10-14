8f0c9b96b2ef8f6d5e125aa08de13ed0cd32062a
Namioko <galaida_anna@inbox.ru>
2020-10-14 14:43:43 +0300

    Change delay time in tooltip

--- dev/components/Tooltip.js
+++ dev/components/Tooltip.js
@@ -11,7 +11,7 @@ export const Tooltip = ({id, targetId, title, content, onCreated}) => {
             content,
             closeOnMouseleave: true,
             onCreated,
-            delayClose: 1000
+            delayClose: 300
         })
         : undefined;
 };
